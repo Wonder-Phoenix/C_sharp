f1f3b8c379d0133befa3f511f6da51f9afcc5765
Wonder-Phoenix <138680353+Wonder-Phoenix@users.noreply.github.com>
2023-09-04 10:58:25 +0200

    *

--- générateurMdp/program.cs
+++ générateurMdp/program.cs
@@ -5,21 +5,12 @@ namespace generateur_mot_de_passe
 {
     class Program
     {
-        
-
         static void Main(string[] args)
         {
             const int NB_MOTS_DE_PASSE = 10;
 
-            // 1 - Demander a longueur du mot de passe (DemanderNombre) int longeur_mot_de_passe = ...
             int longeurMotDePasse = outils.DemanderNombrePositifNonNul("Longueur du mot de passe : ");
-
-            // Vous voulez un mot de passe avec :
-            // 1 - Uniquement des caractères en minuscule
-            // 2 - Des caractères minuscules et majuscules
-            // 3 - Des caractères et des chiffres
-            // 4 - Caractères, chiffres et caractères spéciaux
-
+            
             Console.WriteLine();
             int choixAlphabet = outils.DemanderNombreEntre("Vous voulez un mot de passe avec :\n" +
                 "1 - Uniquement des caractères en minuscule\n" +
@@ -27,12 +18,6 @@ namespace generateur_mot_de_passe
                 "3 - Des caractères et des chiffres\n" +
                 "4 - Caractères, chiffres et caractères spéciaux\n" +
                 "Votre choix : ", 1, 4);
-
-            // poser la question
-            // en fonction du choix alphabet
-
-            // 2 - alphabet "abcd1234"
-            // 3 - comment choisir un caractère aléatoire
 
             string minuscules = "abcdefghijklmnopqrstuvwxyz";
             string majuscules = minuscules.ToUpper();
@@ -64,11 +49,6 @@ namespace generateur_mot_de_passe
 
                 Console.WriteLine("Mot de passe : " + motDePasse);
             }
-            
-
-            // 4 - generer le mot de passe
-            // 5 - Améliorations : choix alphabet
-
         }
     }
 }
